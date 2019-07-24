556f6c69a3794734a4dc0dfda63f4a47d3d5b8fe
athiyagu <thethiyagu@yahoo.com>
2019-07-24 17:59:31 +0530

    Added getAllEmployeeDetails method

--- src/main/java/com/perfm/services/userServices/EmployeeDetailService.java
+++ src/main/java/com/perfm/services/userServices/EmployeeDetailService.java
@@ -1,5 +1,11 @@
 package com.perfm.services.userServices;
 
+import java.util.List;
+
+import com.perfm.services.model.EmployeeDetail;
+
 public interface EmployeeDetailService {
+	
+	List<EmployeeDetail> getAllEmployeeDetails();
 
 }
--- src/main/java/com/perfm/services/userServices/EmployeeDetailServiceImpl.java
+++ src/main/java/com/perfm/services/userServices/EmployeeDetailServiceImpl.java
@@ -1,5 +1,22 @@
 package com.perfm.services.userServices;
 
-public class EmployeeDetailServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.perfm.services.model.EmployeeDetail;
+import com.perfm.services.repository.EmployeeDetailRepository;
+
+public class EmployeeDetailServiceImpl implements EmployeeDetailService {
+
+	@Autowired
+	EmployeeDetailRepository employeeRepo;
+	
+	@Override
+	public List<EmployeeDetail> getAllEmployeeDetails() {
+		List<EmployeeDetail> employeeList = employeeRepo.findAll();
+		return employeeList;
+	}
+
+	
 }
