73067d27b401f8897e4877f8a6a5f794226990f1
Gunalan <gunalan_kuzhandaianchellakkannu@trimble.com>
2019-09-29 13:24:30 +0530

    version updated
    version name added

--- src/main/java/com/perfm/services/test/Test.java
+++ src/main/java/com/perfm/services/test/Test.java
@@ -2,6 +2,7 @@ package com.perfm.services.test;
 
 public class Test {
 
-  public static final double VERSION = 1.0;
+  public static final double VERSION = 1.2;
+  public static final String VERSION_NAME = "1.2";
 
 }
